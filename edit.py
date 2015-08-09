f597765d402650faeb57e8e25be0634ff9b8a8f0
CHJ <c.hartgerink@gmail.com>
2015-08-09 23:13:51 +0200

    Add editlist, test perspsychscience: winning

--- edit.py
+++ edit.py
@@ -1,12 +1,12 @@
 # the journal name which is saved
-journal = "psychologicalscience"
+journal = "persppsychscience"
 
 # lvl1 is the base url
-lvl1 = 'http://pss.sagepub.com/content/by/year/'
+lvl1 = 'http://pps.sagepub.com/content/by/year/'
 
 # links to recognize at lvl1 for lvl2
-lvl1_recog = 'http://pss.sagepub.com/content/by/year/[0-9]{4}'
+lvl1_recog = 'http://pps.sagepub.com/content/by/year/[0-9]{4}'
 # links to recognize at lvl2 for lvl3
-lvl2_recog = 'http://pss.sagepub.com/content/vol[0-9]{1,}/issue[0-9]{1,}/'
+lvl2_recog = 'http://pps.sagepub.com/content/vol[0-9]{1,}/issue[0-9]{1,}/'
 # links to recognize at lvl3 for lvl4
-lvl3_recog = 'http://pss.sagepub.com/content/[0-9]{1,}/[0-9]{1,}/[0-9]{1,}.abstract'
+lvl3_recog = 'http://pps.sagepub.com/content/[0-9]{1,}/[0-9]{1,}/[0-9]{1,}.abstract'
