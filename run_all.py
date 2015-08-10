b41935fd25fa024b5fbbf107468b4f71d33c5523
CHJ <c.hartgerink@gmail.com>
2015-08-10 11:10:05 +0200

    First run at run_all.py

--- run_all.py
+++ run_all.py
@@ -1,16 +1,22 @@
 from spiderer import spiderer
+from time import strftime
 
 journals = np.genfromtxt('journal_list.csv',
  delimiter = ';',
- skiprows = 1,
  dtype = 'str')
 
 # Each list element is a journal
 # Each element within that list follows the order
 # 1. Last updated
-# 2. 
+# 2. Journal ID
+# 3. Journal publisher
+# 4. Journal full name
 
-
+for i in range(0, len(x)):
+	# Run the spiderer for the journal
+	spiderer(journal = journals[i][1], publisher = journals[i][2])
+	# Update the last update time
+	journals[i][0] = strftime("%Y%m%d")
 
 # This updates the last time the links have been generated
 np.savetxt('journal_list.csv',
